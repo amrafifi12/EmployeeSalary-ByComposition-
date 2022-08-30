1509cb9a03455dfbb63431125e91083b6048630c
amrafifi11111@outlook.sa <amrafifi11111@outlook.sa>
2022-08-31 01:26:09 +0200

    Add project files.

---
+++ EmployeeSalary(ByComposition)/Program.cs
@@ -0,0 +1,49 @@
+﻿using EmployeeSalary_ByComposition_;
+
+var choice = 0;
+var Employee = new Base();
+do
+{
+    Console.Clear();
+    choice = ReadChoice(choice);
+    if (choice >= 1 && choice <= 3)
+    {
+        Adds adds = null;
+        switch (choice)
+        {
+            case 1:
+                adds = new Hour();
+                break;
+            case 2:
+                adds = new PartTime();
+                break;
+            case 3:
+                adds = new ExtraDay();
+                break;
+            default:
+                break;
+        }
+        Employee.AddSalary(adds);
+        Console.WriteLine("Press any key to continue");
+    }
+    Console.ReadKey();
+} while (choice != 0);
+Console.WriteLine(Employee);
+Console.ReadKey();
+        
+
+static int ReadChoice(int choice)
+{
+    Console.WriteLine("Available Adds");
+    Console.WriteLine("------------");
+    Console.WriteLine("1. Hour");
+    Console.WriteLine("2. PartTime");
+    Console.WriteLine("3. Extra Day");
+    if (int.TryParse(Console.ReadLine(), out int ch))
+    {
+        choice = ch;
+    }
+
+    return choice;
+}
+    
---
+++ EmployeeSalary(ByComposition)/base.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSalary_ByComposition_
+{
+    internal class Base
+    {
+        public virtual decimal baseSalary => 1000;
+        public List<Adds> adds { get; private set; } = new List<Adds>();
+        public override string ToString()
+        {
+            string result = $"\n\tYour Base Salary is: {baseSalary}";
+            foreach (Adds item in adds)
+            {
+                result += $"\n\t{item.title} ({item.salary.ToString("C")})";
+            }
+            result += "\n------------------------------------------";
+            result += $"\n Total:({CalculateTotal().ToString("C")})";
+            return result;
+        }
+        public void AddSalary(Adds add)
+        {
+            adds.Add(add);
+        }
+        public decimal CalculateTotal()
+        {
+            decimal total = 0;
+            total=baseSalary;
+            foreach (Adds add in adds)
+            {
+                total += add.salary;
+            }
+            return total;
+        }
+
+    }
+    public interface Adds
+    {
+        string title { get; }
+        decimal salary { get; }
+    }
+    public class PartTime : Adds
+    {
+        public string title => nameof(PartTime);
+
+        public decimal salary => 100;
+    }
+    public class Hour : Adds
+    {
+        public string title => nameof(Hour);
+
+        public decimal salary => 15;
+    }
+    public class ExtraDay : Adds
+    {
+        public string title => nameof(ExtraDay);
+
+        public decimal salary => 150;
+    }
+}
